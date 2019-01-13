c62c5d364834ec91190570d86a5e5df1bbaf6b75
jude-lafitteIII <judewrs@gmail.com>
2019-01-13 18:24:42 +0000

    reverted

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ fn main() {
 
             if let Some(t) = text {
                 let m = Message {
-                    content: format!("\u{200B}{}", t),
+                    content: t,
                 };
 
                 let req = send_message(format!("{}/channels/{}/messages", URL, channel), serde_json::to_string(&m).unwrap(), &token, &req_client);
